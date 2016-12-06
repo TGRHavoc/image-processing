4ce5cfa2b4ade22c60e2fdfc770e386381008c85
Jordan Dalton <jordan.8474@gmail.com>
2016-12-06 13:21:12 +0000

    Updated compile script
    It now creates the output directory if it doesn't exist.

--- compile.sh
+++ compile.sh
@@ -2,6 +2,12 @@
 
 echo "Starting..."
 
+# Make sure "output" exists..
+if [ ! -d "output/" ]; then
+    mkdir output/
+fi
+
+# Compile the program !!
 g++ -std=c++14 -w *.cpp *.h -o output/a.out
 
 echo "Done"
